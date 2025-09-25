af6c220ce82a10f1607968aafb3745129149ac06
= <anirudh@madmen.in>
2025-09-25 11:37:03 +0530

    ui changes

--- src/app/(admin)/dashboard/analytics/components/Stats1.jsx
+++ src/app/(admin)/dashboard/analytics/components/Stats1.jsx
@@ -1,13 +1,13 @@
 import IconifyIcon from '@/components/wrappers/IconifyIcon'
 import { Card, CardBody, Col, Row } from 'react-bootstrap'
 
-const StatCard = ({ count }) => {
+const StatCard = ({ count, title }) => {
   return (
     <Card>
       <CardBody>
         <Row>
           <Col xs={6} className="text-start">
-            <p className="mb-0  text-capitalize">Total Employees</p>
+            <p className="mb-0 text-capitalize">{title}</p>
             <h3 className="text-dark mt-2 mb-0 text-truncate">{count}</h3>
           </Col>
           <Col xs={6}></Col>
@@ -17,14 +17,14 @@ const StatCard = ({ count }) => {
   )
 }
 
-const Stats1 = ({ data }) => {
+const Stats1 = ({ data, title }) => {
   // data is your full response object
   const totalEmployees = data?.length || 0
 
   return (
     <Row>
       <Col md={12} xl={12}>
-        <StatCard count={totalEmployees} />
+        <StatCard count={totalEmployees} title={title} />
       </Col>
     </Row>
   )
